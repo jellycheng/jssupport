13a44f4fa900ef2fb6d1c9602890b6662052ec9a
js <js@qq.com>
2023-05-02 22:12:55 +0800

    add walk func

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 'use strict';
 
+function walk(obj, path, initMissing) {
+  if (path) {
+    let ary = path.split('.');
+    while (ary.length) {
+      let k = ary.shift();
+      if (initMissing && obj[k] == null) {
+        obj[k] = {};
+        obj = obj[k];
+      } else if (k in obj) {
+        obj = obj[k];
+      } else {
+        throw new Error("配置不存在：'" + path + "'");
+      }
+    }
+  }
+
+  return obj;
+}
+
 function hi(str) {
   console.log(str || "welcome to jssupport");
 }
 
+module.exports = walk;
 module.exports = hi;
